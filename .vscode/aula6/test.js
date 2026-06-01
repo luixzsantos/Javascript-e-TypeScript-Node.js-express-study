b83edd87fdcff0bec4c2fd8291d6eeba6d7c337c
luixzsantos <luizfernandotec.link2@gmail.com>
2026-05-31 22:57:41 -0300

    Update

---
+++ .vscode/aula6/test.js
@@ -0,0 +1,11 @@
+let nome;
+nome = "Qualquer valor"; // Atribuindo um valor a variavel
+console.log(nome); // Qualquer valor
+// Ele reconhece o valor da variavel, mas como ela nao tem valor, ele mostra undefined
+nome = "Rebeca";
+console.log(nome); // Rebeca
+/*
+let nome;
+console.log('teste'); // undefined
+
+Não funciona porque a variavel nome nao tem valor, entao ele mostra undefined
