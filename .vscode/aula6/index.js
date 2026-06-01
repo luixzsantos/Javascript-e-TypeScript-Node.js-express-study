54b526a6c50bc9b9097c5bed01996b4015028b22
luixzsantos <luizfernandotec.link2@gmail.com>
2026-05-31 22:17:15 -0300

    learning basics of js and ts

---
+++ .vscode/aula6/index.js
@@ -0,0 +1,8 @@
+// Criar variaveis
+let nome = "Rebeca"; // String
+
+console.log(nome, "nasceu em tal ano");
+console.log(nome, "trabalha como backoffice");
+
+// Colocando a variavel com o nome dentro da string ele e substituido pelo valor da variavel
+
