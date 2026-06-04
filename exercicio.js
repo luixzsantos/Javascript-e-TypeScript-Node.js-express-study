4a12ec2fad47e047c99ab8cd1ff098c1351ddc45
luixzsantos <luizfernandotec.link2@gmail.com>
2026-06-04 00:03:55 -0300

    Coisando

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+const nome = ("Luiz Fernando");
+const altura = 1.74;
+const idade = 22;
+const peso = 74;
+let imc = peso / (altura * altura);
+
+console.log(nome, "tenho", altura, "de altura", "tenho", idade, "anos", "e peso", peso, "kg", "e meu IMC é", imc);
---
+++ numconst.js
@@ -0,0 +1,10 @@
+// + = * /
+const primeironumero = 5; // Number
+const segundonumero = 10;
+const resultado = primeironumero * segundonumero;
+const resultadoduplicado = resultado * 2;
+let resultadotriplicado = resultado * 3;
+resultadotriplicado = resultado + 4;
+
+console.log(typeof primeironumero);
+// Dentro de aspas = string ou texto | Number = número
