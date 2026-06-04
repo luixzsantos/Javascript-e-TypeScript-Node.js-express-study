0cf07f22d3c20bba18fdc304adb7cce55a87d8ff
luixzsantos <luizfernandotec.link2@gmail.com>
2026-06-04 00:42:41 -0300

    learning

---
+++ aula9/index.js
@@ -0,0 +1,17 @@
+// Strigs
+// Numbers
+
+const name = "John"; //  Constante Strings
+const name1 = 'John'; //  Constante Strings
+
+let n = 123; // Numbers
+let n1 = 12.4;
+
+const n = 123; // Numbers
+const n1 = 12.4;
+
+let nomeAluno; // undefined
+let nomeAluno1 = null; // Nulo, vai querer descobrir o tipo da variável, mas não tem nada lá, é nulo
+
+console.log(typeof name); // type do python, descobre o tipo da variável
+
